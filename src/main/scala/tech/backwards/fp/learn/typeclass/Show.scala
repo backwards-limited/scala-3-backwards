bdfe7c812cba868b0acb247e364aeaf3cd4f807d
David Ainslie <dainslie@gmail.com>
2022-10-04 21:35:36 +0100

    Learn FP refactor - Show type class

---
+++ src/main/scala/tech/backwards/fp/learn/typeclass/Show.scala
@@ -0,0 +1,33 @@
+package tech.backwards.fp.learn.typeclass
+
+import cats.implicits._
+
+trait Show[A] {
+  def show(x: A): String
+}
+
+object Show extends ShowGivens {
+  extension[A: Show](x: A) {
+    def show: String =
+      summon[Show[A]].show(x)
+  }
+}
+
+trait ShowGivens {
+  import tech.backwards.fp.learn.typeclass.Show.*
+
+  given Show[Int] with {
+    def show(x: Int): String =
+      x.toString
+  }
+
+  given Show[Double] with {
+    def show(x: Double): String =
+      x.toString
+  }
+
+  given [A: Show]: Show[List[A]] with {
+    def show(xs: List[A]): String =
+      xs.map(_.show).mkString("[", ", ", "]")
+  }
+}
