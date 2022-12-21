837c96a5e31a4c4b00f70a2e194abd4fe8b8a089
David Ainslie <dainslie@gmail.com>
2022-12-21 20:51:05 +0000

    Learn FP refactor - State Functor

---
+++ src/main/scala/tech/backwards/fp/learn/functor/State.scala
@@ -0,0 +1,28 @@
+package tech.backwards.fp.learn.functor
+
+final case class State[S, A](run: S => (S, A)) {
+  def exec(s: S): S =
+    run(s)._1
+
+  def eval(s: S): A =
+    run(s)._2
+}
+
+object State {
+  def put[S](s: S): State[S, Unit] =
+    State(_ => s -> ())
+
+  def get[S]: State[S, S] =
+    State(s => s -> s)
+
+  def modify[S](f: S => S): State[S, Unit] =
+    State(s => f(s) -> ())
+    
+  given [S]: Functor[[A] =>> State[S, A]] with {
+    def fmap[A, B](fa: State[S, A])(f: A => B): State[S, B] =
+      State(s =>
+        val (ss, a) = fa.run(s)
+        ss -> f(a)
+      )
+  }
+}
