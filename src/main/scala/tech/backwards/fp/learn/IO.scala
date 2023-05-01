bd626351a05aa88e9c107d45a9101754d1aa8393
David Ainslie <dainslie@gmail.com>
2023-05-01 20:26:57 +0100

    IO with Functor, Monad and Applicative

---
+++ src/main/scala/tech/backwards/fp/learn/IO.scala
@@ -0,0 +1,32 @@
+package tech.backwards.fp.learn
+
+sealed abstract case class IO[A] private(thunk: () => A) {
+  def unsafeRunSync(): A =
+    thunk()
+}
+
+object IO {
+  def apply[A](thunk: => A): IO[A] =
+    new IO(() => thunk) {}
+
+  given functorIO: Functor[IO] with {
+    def fmap[A, B](fa: IO[A])(f: A => B): IO[B] =
+      IO(f(fa.unsafeRunSync()))
+  }
+
+  given monadIO: Monad[IO] with {
+    def pure[A](a: A): IO[A] =
+      IO(a)
+
+    def flatMap[A, B](fa: IO[A])(f: A => IO[B]): IO[B] =
+      f(fa.unsafeRunSync())
+  }
+
+  given Applicative[IO] with {
+    def pure[A](a: A): IO[A] =
+      monadIO.pure(a)
+
+    def ap[A, B](ff: IO[A => B])(fa: IO[A]): IO[B] =
+      functorIO.fmap(fa)(ff.unsafeRunSync())
+  }
+}
---
+++ src/test/scala/tech/backwards/fp/learn/IOSuite.scala
@@ -0,0 +1,129 @@
+package tech.backwards.fp.learn
+
+import java.lang.System.lineSeparator
+import java.util.concurrent.atomic.{AtomicInteger, AtomicReference}
+import munit.ScalaCheckSuite
+import org.scalacheck.Test
+
+class IOSuite extends ScalaCheckSuite {
+  override protected def scalaCheckTestParameters: Test.Parameters =
+    super.scalaCheckTestParameters.withMinSuccessfulTests(100).withMaxDiscardRatio(10)
+
+  property("Run IO") {
+    val io: IO[String] =
+      IO("foo")
+
+    assertEquals(io.unsafeRunSync(), "foo")
+  }
+
+  property("Run IO twice") {
+    val callCount: AtomicInteger =
+      new AtomicInteger(0)
+
+    val io: IO[String] =
+      IO {
+        callCount.incrementAndGet()
+        "foo"
+      }
+
+    assertEquals(io.unsafeRunSync(), "foo")
+    assertEquals(callCount.get(), 1)
+
+    assertEquals(io.unsafeRunSync(), "foo")
+    assertEquals(callCount.get(), 2)
+  }
+
+  property("IO Functor") {
+    val io: IO[String] =
+      IO("foo")
+
+    val mappedIO: IO[String] =
+      Functor[IO].fmap(io)(_ + "bar")
+
+    assertEquals(mappedIO.unsafeRunSync(), "foobar")
+  }
+
+  property("IO Functor syntax") {
+    import tech.backwards.fp.learn.Functor.syntax.*
+
+    val io: IO[String] =
+      IO("foo")
+
+    val mappedIO: IO[String] =
+      io `<$>` (_ + "bar")
+
+    assertEquals(mappedIO.unsafeRunSync(), "foobar")
+  }
+
+  property("IO Monad") {
+    val io: IO[String] =
+      Monad[IO].pure("foo")
+
+    val flatMappedIO: IO[String] =
+      Monad[IO].flatMap(io)(s => IO(s + "bar"))
+
+    assertEquals(flatMappedIO.unsafeRunSync(), "foobar")
+  }
+
+  property("IO Monad syntax") {
+    import tech.backwards.fp.learn.Monad.syntax.*
+
+    val io: IO[String] =
+      "foo".pure[IO]
+
+    val flatMappedIO: IO[String] =
+      io >>= (s => IO(s + "bar"))
+
+    assertEquals(flatMappedIO.unsafeRunSync(), "foobar")
+  }
+
+  property("IO Applicative") {
+    val io: IO[String] =
+      Applicative[IO].pure("foo")
+
+    val apedIO: IO[Int] =
+      Applicative[IO].ap(IO((s: String) => s.length))(io)
+
+    assertEquals(apedIO.unsafeRunSync(), 3)
+  }
+
+  property("IO simulate reading from a file twice with an update") {
+    import tech.backwards.fp.learn.Functor.syntax.*
+    import tech.backwards.fp.learn.Monad.syntax.*
+
+    val fileContents: AtomicReference[String] =
+      new AtomicReference[String]("foo")
+
+    val readFile: IO[String] =
+      IO(fileContents.get())
+
+    val appendFile: String => IO[Unit] =
+      contents => IO(fileContents.getAndUpdate(_ + lineSeparator() + contents))
+
+    val program: IO[(String, String)] =
+      for {
+        contents <- readFile
+        _ <- appendFile("bar")
+        updatedContents <- readFile
+      } yield (contents, updatedContents)
+
+    val (originalContents, updatedContents) =
+      program.unsafeRunSync()
+
+    assertEquals(originalContents, "foo")
+    assertEquals(updatedContents, "foo" + lineSeparator() + "bar")
+  }
+
+  property("IO Applicative function") {
+    import tech.backwards.fp.learn.Applicative.syntax.*
+    import tech.backwards.fp.learn.Functor.syntax.function.*
+
+    def add(x: Int, y: Int, z: Int): Int =
+      x + y + z
+
+    val program: IO[Int] =
+      (add _).curried `<$>` IO(1) <*> IO(2) <*> IO(3)
+
+    assertEquals(program.unsafeRunSync(), 6)
+  }
+}
