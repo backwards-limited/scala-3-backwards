a3c8b1abbf22fce355afc685f9a2319c4f17e9ce
David Ainslie <dainslie@gmail.com>
2023-05-08 16:50:01 +0100

    Clean up

--- src/test/scala/tech/backwards/fp/learn/IdMonadTransformerMaybeSuite.scala
+++ src/test/scala/tech/backwards/fp/learn/IdMonadTransformerMaybeSuite.scala
@@ -3,10 +3,38 @@ package tech.backwards.fp.learn
 import munit.ScalaCheckSuite
 import org.scalacheck.Test
 
+/**
+ * First of the following IdT suites:
+ *  - IdMonadTransformerMaybeSuite
+ *  - IdMonadTransformerDisjunctionSuite
+ *  - IdMonadTransformerListSuite
+ *  - IdMonadTransformerStateSuite
+ *  - IdMonadTransformerWriterSuite
+ */
 class IdMonadTransformerMaybeSuite extends ScalaCheckSuite {
   override protected def scalaCheckTestParameters: Test.Parameters =
     super.scalaCheckTestParameters.withMinSuccessfulTests(100).withMaxDiscardRatio(10)
 
+  property("IdT") {
+    val transformer: IdT[Maybe, Int] =
+      IdT(Just(Id(10)))
+
+    assertEquals(
+      transformer.value,
+      Just(Id(10))
+    )
+  }
+
+  property("IdT pure") {
+    val transformer: IdT[Maybe, Int] =
+      IdT.pure[Maybe, Int](10)
+
+    assertEquals(
+      transformer.value,
+      Just(Id(10))
+    )
+  }
+  
   property("IdT lift") {
     val transformer: IdT[Maybe, Int] =
       IdT.lift(Just(10))
